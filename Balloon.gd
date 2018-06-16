70522e0a32a0b92f787dc34048160174d5f7b770
Bhavani Shankar <ebhavanishankar@gmail.com>
2018-06-17 00:38:41 +0530

    separate the scene into individual scenes

--- Balloon.gd
+++ Balloon.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 const INIT_VELOCITY = Vector2(400, -200)
-const FRICTION = Vector2(0.95, 1)
+const FRICTION = Vector2(0.99, 1)
 var velocity = Vector2(0, 0)
 
 func _ready():
