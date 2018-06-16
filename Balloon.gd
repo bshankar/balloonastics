8d57228425ffacb909c487b15d25078178b339ce
Bhavani Shankar <ebhavanishankar@gmail.com>
2018-06-16 19:25:14 +0530

    add initial velocity, friction along x-axis to balloon

---
+++ Balloon.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+const INIT_VELOCITY = Vector2(400, -200)
+const FRICTION = Vector2(0.95, 1)
+var velocity = Vector2(0, 0)
+
+func _ready():
+	velocity = INIT_VELOCITY
+	
+func _physics_process(delta):
+	velocity *= FRICTION
+	velocity = move_and_slide(velocity)
